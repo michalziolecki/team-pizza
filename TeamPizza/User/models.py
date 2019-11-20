01bed4dfde21a7561ea122f3e87a472941d16be3
michal.ziolecki <michal.ziolecki@op.pl>
2019-11-21 00:13:11 +0100

    First template for users models

--- TeamPizza/User/models.py
+++ TeamPizza/User/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+# Main user class
+class User(models.Model):
+    ADMIN = 'A'
+    MODERATOR = 'M'
+    USER = 'U'
+    ROLES = [
+        (ADMIN, 'Admin'),
+        (MODERATOR, 'Moderator'),
+        (USER, 'User')
+    ]
+
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    mail = models.CharField(max_length=100)
+    password_hash = models.CharField(max_length=100)
+    role = models.CharField(max_length=1, choices=ROLES, default=USER)
+
+
+# one to many relation
+class LoginInformation(models.Model):
+    last_login = models.DateTimeField(null=True)
+    ip_login = models.IPAddressField()
+
+
+# one to one relation
+class LastOrder(models.Model):
+    pieces_number = models.IntegerField(default=0)
