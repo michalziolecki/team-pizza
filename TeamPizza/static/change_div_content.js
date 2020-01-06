f701157cb9fa9fce692648663e730873663e5a74
michal.ziolecki <michal.ziolecki@op.pl>
2020-01-06 02:36:35 +0100

    Tabs style finished + js method to change tabs background added

--- TeamPizza/static/change_div_content.js
+++ TeamPizza/static/change_div_content.js
@@ -1,28 +1,62 @@
+const createBackgroundTabColor = (updateColor, securityColor, removeColor ) => {
+    var updateTab = document.getElementById('update-tab');
+    var securityTab = document.getElementById('security-tab');
+    var removeTab = document.getElementById('remove-tab');
+    updateTab.style.backgroundColor = updateColor;
+    securityTab.style.backgroundColor = securityColor;
+    removeTab.style.backgroundColor = removeColor;
+};
+
 const changeDivToUpdate = () => {
-    var updateTab = document.getElementById('update-user-form');
-    var securityTab = document.getElementById('last-login-table');
-    var removeTab = document.getElementById('delete-user-form');
-    console.log("display test");
-    console.log(updateTab.style.display);
-    updateTab.style.display = "block";
-    securityTab.style.display = "none";
-    removeTab.style.display = "none";
+    var updateContentTab = document.getElementById('update-user-form');
+    var securityContentTab = document.getElementById('last-login-table');
+    var removeContentTab = document.getElementById('delete-user-form');
+    updateContentTab.style.display = "block";
+    securityContentTab.style.display = "none";
+    removeContentTab.style.display = "none";
+
+    createBackgroundTabColor('#ccc', '', '');
+    // var updateTab = document.getElementById('update-tab');
+    // var securityTab = document.getElementById('security-tab');
+    // var removeTab = document.getElementById('remove-tab');
+    // updateTab.style.backgroundColor = '#ccc';
+    // securityTab.style.backgroundColor = '';
+    // removeTab.style.backgroundColor = '';
 };
 
 const changeDivToSecurity = () => {
-    var updateTab = document.getElementById('update-user-form');
-    var securityTab = document.getElementById('last-login-table');
-    var removeTab = document.getElementById('delete-user-form');
-    updateTab.style.display = "none";
-    securityTab.style.display = "block";
-    removeTab.style.display = "none";
+    var updateContentTab = document.getElementById('update-user-form');
+    var securityContentTab = document.getElementById('last-login-table');
+    var removeContentTab = document.getElementById('delete-user-form');
+    updateContentTab.style.display = "none";
+    securityContentTab.style.display = "block";
+    removeContentTab.style.display = "none";
+
+    createBackgroundTabColor('', '#ccc', '');
+
+    // var updateTab = document.getElementById('update-tab');
+    // var securityTab = document.getElementById('security-tab');
+    // var removeTab = document.getElementById('remove-tab');
+    // updateTab.style.backgroundColor = '';
+    // securityTab.style.backgroundColor = '#ccc';
+    // removeTab.style.backgroundColor = '';
 };
 
 const changeDivToRemove = () => {
-    var updateTab = document.getElementById('update-user-form');
-    var securityTab = document.getElementById('last-login-table');
-    var removeTab = document.getElementById('delete-user-form');
-    updateTab.style.display = "none";
-    securityTab.style.display = "none";
-    removeTab.style.display = "block";
+    var updateContentTab = document.getElementById('update-user-form');
+    var securityContentTab = document.getElementById('last-login-table');
+    var removeContentTab = document.getElementById('delete-user-form');
+    updateContentTab.style.display = "none";
+    securityContentTab.style.display = "none";
+    removeContentTab.style.display = "block";
+
+    createBackgroundTabColor('', '', '#ccc');
+
+    // var updateTab = document.getElementById('update-tab');
+    // var securityTab = document.getElementById('security-tab');
+    // var removeTab = document.getElementById('remove-tab');
+    // updateTab.style.backgroundColor = '';
+    // securityTab.style.backgroundColor = '';
+    // removeTab.style.backgroundColor = '#ccc';
 };
+
