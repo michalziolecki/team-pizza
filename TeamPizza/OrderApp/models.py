7a9693ec448f146754f4cda64842d6b85828b47b
michal.ziolecki <michal.ziolecki@op.pl>
2020-01-09 23:16:52 +0100

    First version of DB models - ordering logic

--- TeamPizza/OrderApp/models.py
+++ TeamPizza/OrderApp/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    order_owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    is_open = models.BooleanField(default=True, null=False)
+    open_time = models.DateTimeField(null=False)
+    close_time = models.BooleanField(null=True)
+
+
+class ContributionOrder(models.Model):
+    contribution_owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    pieces_number = models.IntegerField(null=False)
+    add_contr_time = models.DateTimeField(null=False)
+    was_updated = models.BooleanField(default=False)
+
